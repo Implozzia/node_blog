9990afac4942f2868dc9391a1323ea514b0b0b6c
ruslan_vaf <ruslan.vafin.93@gmail.com>
2020-11-03 17:46:05 +0300

    connect to mongo db

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const config = require('./config/db')
 
 const app = express();
 
 const port = 3000;
+
+mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected', () => {
+    console.log("Successful connection to database")
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Not successful connection to database" + err)
+});
 
 app.listen(port, () => {
     console.log("Server started at: " + port)
